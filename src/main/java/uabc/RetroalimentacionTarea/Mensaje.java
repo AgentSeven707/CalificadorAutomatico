95011ce02c1fb5e74d87e4597b72679684d7974e
Princ <Princ@SEVEN>
2022-09-04 18:42:31 -0700

    Version 3

--- src/main/java/uabc/RetroalimentacionTarea/Mensaje.java
+++ src/main/java/uabc/RetroalimentacionTarea/Mensaje.java
@@ -10,11 +10,11 @@ public class Mensaje {
     public String mensaje(int op, String tema){
         String mensaje = "";
         if (op == 1){
-            mensaje = "La tarea cumple con todos los criterios: "+tema+": 100"; 
+            mensaje = "La tarea sobre "+tema+" cumple con todos los criterios: 100"; 
         } else if (op == 2){
-            mensaje = "La tarea solo cumple con la mitad de los criterios: "+tema+": 50";
+            mensaje = "La tarea sobre "+tema+" solo cumple con la mitad de los criterios: 50";
         } else if (op == 3){
-            mensaje = "La tarea no cumple con los criterios: "+tema+": 0";
+            mensaje = "La tarea sobre "+tema+" no cumple con los criterios: 0";
         }
         return mensaje; 
     }
